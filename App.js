0504fbd32115dc06d995d22c7e616b87a9720768
Naflan-MNM <naflanm084@gmail.com>
2025-01-16 03:04:05 +0530

    intergreat navigation

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LandingPage from './ui/LandingPage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import OpenPage2 from './ui/OpenPage2';
+import StartPage from './ui/StartPage';
+import StartPage2 from './ui/StartPage2';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LandingPage/>
-      <StatusBar 
-        backgroundColor='#80a326'/* style="auto" */ />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LandingPage'>
+        <Stack.Screen 
+          name="LandingPage" 
+          component={LandingPage} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="OpenPage2" 
+          component={OpenPage2} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="StartPage" 
+          component={StartPage} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="StartPage2" 
+          component={StartPage2} 
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
