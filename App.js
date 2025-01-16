5e4a46cfa3168915bf956fe8f7b9fc2b7b5b4819
Naflan-MNM <naflanm084@gmail.com>
2025-01-16 21:43:13 +0530

    adding new roots

--- App.js
+++ App.js
@@ -1,38 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LandingPage from './ui/LandingPage';
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import LandingPage from './ui/LandingPage';
 import OpenPage2 from './ui/OpenPage2';
 import StartPage from './ui/StartPage';
 import StartPage2 from './ui/StartPage2';
+import MainStartPage from './ui/MainStartPage';
+import StoryPage from './ui/StoryPage';
+import SavePage from './ui/SavePage';
+import ProfilePage from './ui/ProfilePage';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='LandingPage'>
-        <Stack.Screen 
-          name="LandingPage" 
-          component={LandingPage} 
+      <Stack.Navigator initialRouteName="MainStartPage">
+        <Stack.Screen
+          name="LandingPage"
+          component={LandingPage}
           options={{ headerShown: false }}
         />
-        <Stack.Screen 
-          name="OpenPage2" 
-          component={OpenPage2} 
+        <Stack.Screen
+          name="OpenPage2"
+          component={OpenPage2}
           options={{ headerShown: false }}
         />
-        <Stack.Screen 
-          name="StartPage" 
-          component={StartPage} 
+        <Stack.Screen
+          name="StartPage"
+          component={StartPage}
           options={{ headerShown: false }}
         />
-        <Stack.Screen 
-          name="StartPage2" 
-          component={StartPage2} 
+        <Stack.Screen
+          name="MainStartPage"
+          component={MainStartPage}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="StoryPage" component={StoryPage} />
+        <Stack.Screen name="SavePage" component={SavePage} />
+        <Stack.Screen name="ProfilePage" component={ProfilePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
